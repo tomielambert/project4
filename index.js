dd258d0f9b6b6e5d0047b4b8ebe190f759f77ae9
tomielambert <37849246+tomielambert@users.noreply.github.com>
2018-05-31 10:24:55 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+<a onclick="document.getElementById('yourAudioTag').play(<audio controls autoplay>
+   <source src="images/zelda_main_theme.mp3" type="audio/mpeg">
+   </audio>);">
+     <img src="play_button.png">
+</a>
